ab30f32498aa4a2bdc188c0d26eb0ef1d58f6f8e
EC2 Default User <ec2-user@ip-172-31-16-194.ec2.internal>
2021-03-02 08:39:24 +0000

    [add]権限とバリデーションの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '29ab8aa7cea6008f030530b8aa24ca1550d6e026d87d02c954b1c048e7b4db9a18e3dd712957fc1f6ffc1465ff5dc708094d0aacae2fc6cf10203fe055b8c9bf'
+
